07ea29d17299e9fd144e4ce562369664ea488987
Charlene <woodcn14@gmail.com>
2016-06-04 00:14:49 -0400

    Add test button

--- app/src/main/java/woodcn/com/taco/MainActivity.java
+++ app/src/main/java/woodcn/com/taco/MainActivity.java
@@ -1,31 +1,33 @@
 package woodcn.com.taco;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button eatToyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        eatToyButton = (Button) findViewById(R.id.eat_toy_button);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        eatToyButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                toolbar.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.pink));
             }
         });
+
     }
 
     @Override
